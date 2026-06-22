94b60d7d0165a57aa6562ae8e68376abb5ef457b
hasilm <58154546+hasilm@users.noreply.github.com>
2026-06-21 20:41:51 -0400

    Update train.py

--- Fearthquake_predictNextDate/model_building/train.py
+++ Fearthquake_predictNextDate/model_building/train.py
@@ -19,6 +19,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import RandomizedSearchCV
+pip install xgboost
+
 from xgboost import XGBRegressor
 
 # Train Spatial Timeline Model
