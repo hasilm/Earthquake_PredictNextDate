aa91a43116c475749d2317f6109128df892ec0f1
hasilm <58154546+hasilm@users.noreply.github.com>
2026-06-22 19:44:22 -0400

    Update app.py

--- Fearthquake_predictNextDate/deployment/app.py
+++ Fearthquake_predictNextDate/deployment/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from huggingface_hub import hf_hub_download
 import joblib
 from sklearn.cluster import KMeans
+from xgboost import XGBRegressor
 
 Folder_name="Fearthquake_predictNextDate"
 HF_username="hasilm1"
