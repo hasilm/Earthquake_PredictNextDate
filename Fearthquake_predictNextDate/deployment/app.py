3afe9a4414516786815bec17f894faa7a640c6b1
hasilm <58154546+hasilm@users.noreply.github.com>
2026-07-05 17:24:16 -0400

    Update app.py

--- Fearthquake_predictNextDate/deployment/app.py
+++ Fearthquake_predictNextDate/deployment/app.py
@@ -87,8 +87,8 @@ else:
 df_path = "hf://datasets/"+str(HF_username)+"/"+str(App_name)+"/df.csv"                      # enter the Hugging Face username here
 df = pd.read_csv(df_path)
 
-df_path = "hf://datasets/"+str(HF_username)+"/"+str(App_name)+"/df.csv"                      # enter the Hugging Face username here
-df = pd.read_csv(df_path)
+X_spatial_path = "hf://datasets/"+str(HF_username)+"/"+str(App_name)+"/X_spatial.csv"                      # enter the Hugging Face username here
+X_spatial = pd.read_csv(X_spatial_path)
 
 spatial_kmeans = KMeans(n_clusters=25, random_state=42, n_init='auto')
 df['spatial_cluster_id'] = spatial_kmeans.fit_predict(df[['latitude', 'longitude']])
