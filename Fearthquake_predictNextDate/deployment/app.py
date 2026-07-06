c0a48f29b6e2a4b7b3fa594a411fa5b0bbc8af4c
hasilm <58154546+hasilm@users.noreply.github.com>
2026-07-06 00:13:02 -0400

    Update app.py

--- Fearthquake_predictNextDate/deployment/app.py
+++ Fearthquake_predictNextDate/deployment/app.py
@@ -580,7 +580,7 @@ def formatOutput(lat,lon,det):
     st.markdown("<span style='font-size: 10px;'>"+str(ret)+"</span>", unsafe_allow_html=True)
     ret="<b>RECENT EARTHQUAKE:</b>"
     st.markdown("<span style='background-color: #FF4B4B;font-size: 18px;'>"+str(ret)+"</span>", unsafe_allow_html=True)
-    ret="<b><i>which is close to the address entered</i></b>"
+    ret="<b><i>red[which is close to the address entered]</i></b>"
     st.markdown("<p style='text-align: left; margin-bottom: 0px;font-size: 12px;'>"+str(ret)+"</p>", unsafe_allow_html=True)
 
     ret="Address   : <b>"+str(add)+"</>"
