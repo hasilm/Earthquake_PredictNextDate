f6faca320205555a542352a3c2ec7f71f6266fde
hasilm <58154546+hasilm@users.noreply.github.com>
2026-08-01 23:17:19 -0400

    Update app.py

--- Fearthquake_predictNextDate/deployment/app.py
+++ Fearthquake_predictNextDate/deployment/app.py
@@ -20,9 +20,15 @@ Model_name2="earthquake_predict_mag_model_v1.joblib"
 #model_path = hf_hub_download(repo_id=str(HF_username)+"/"+str(App_name), filename=Model_name2) # enter the Hugging Face username here
 #model_mag = joblib.load(model_path)
 
-X_spatial = hf_hub_download(repo_id=str(HF_username)+"/"+str(App_name), filename="X_spatial") # enter the Hugging Face username here
-y_days = hf_hub_download(repo_id=str(HF_username)+"/"+str(App_name), filename="y_days") # enter the Hugging Face username here
-y_mag = hf_hub_download(repo_id=str(HF_username)+"/"+str(App_name), filename="y_mag") # enter the Hugging Face username here
+repo_url="https://huggingface.co/datasets/hasilm1/Earthquake_PredictNextDate/tree/main"
+spatial_url = f"{repo_url}/X_spatial.csv"
+X_spatial = pd.read_csv(spatial_url)
+
+spatial_url = f"{repo_url}/y_days.csv"
+y_days = pd.read_csv(spatial_url)
+
+spatial_url = f"{repo_url}/y_mag.csv"
+y_mag = pd.read_csv(spatial_url)
 
 model_days = XGBRegressor(n_estimators=400, max_depth=6, learning_rate=0.03, random_state=42)
 model_days.fit(X_spatial, y_days)
